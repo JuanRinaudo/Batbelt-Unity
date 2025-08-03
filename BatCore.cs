4c1723b95ec04a39442e6460c4524873f13f53f6
Juan Rinaudo <jg>
2025-08-03 05:09:35 +0200

    Definition change

--- BatCore.cs
+++ BatCore.cs
@@ -5,21 +5,21 @@ public class BatCore
     
     public static void Log(string message)
     {
-#if DEBUG_ON || UNITY_EDITOR
+#if DEBUG_BUILD || UNITY_EDITOR
         Debug.Log(message);
 #endif
     }
     
     public static void LogWarning(string message)
     {
-#if DEBUG_ON || UNITY_EDITOR
+#if DEBUG_BUILD || UNITY_EDITOR
         Debug.LogWarning(message);
 #endif
     }
     
     public static void LogError(string message)
     {
-#if DEBUG_ON || UNITY_EDITOR
+#if DEBUG_BUILD || UNITY_EDITOR
         Debug.LogError(message);
 #endif
     }
